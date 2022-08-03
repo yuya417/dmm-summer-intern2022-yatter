78f0ee14d241a55b3fc64a61be138982aee279d7
鎌田裕也 <yk0417yuya@gmail.com>
2022-08-03 17:28:56 +0900

    アカウント作成の実装

--- app/dao/account.go
+++ app/dao/account.go
@@ -37,3 +37,17 @@ func (r *account) FindByUsername(ctx context.Context, username string) (*object.
 
 	return entity, nil
 }
+
+func (r account) CreateUser(ctx context.Context, user *object.Account) (*object.Account, error) {
+    if _, err := r.db.ExecContext(ctx, "insert into account (username, password_hash) value (?, ?)", user.Username, user.PasswordHash); err != nil {
+        return nil, fmt.Errorf("%w", err)
+    }
+    if err := r.db.QueryRowxContext(ctx, "select * from account where username = ?", user.Username).StructScan(user); err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return nil, nil
+        }
+
+        return nil, fmt.Errorf("%w", err)
+    }
+    return user, nil
+}
--- app/domain/repository/account.go
+++ app/domain/repository/account.go
@@ -10,4 +10,5 @@ type Account interface {
 	// Fetch account which has specified username
 	FindByUsername(ctx context.Context, username string) (*object.Account, error)
 	// TODO: Add Other APIs
+	CreateUser(ctx context.Context, user *object.Account) (*object.Account, error)
 }
