7e47604511a519fb1c8dbb7e4cd445e590fd1f88
鎌田裕也 <yk0417yuya@gmail.com>
2022-08-04 15:13:22 +0900

    Statusを取得する処理を追加

--- app/dao/status.go
+++ app/dao/status.go
@@ -23,20 +23,20 @@ func NewStatus(db *sqlx.DB) repository.Status {
 	return &status{db: db}
 }
 
-// FindBystatus : ユーザ名からユーザを取得
-// func (r *account) FindByUsername(ctx context.Context, username string) (*object.Account, error) {
-// 	entity := new(object.Account)
-// 	err := r.db.QueryRowxContext(ctx, "select * from account where username = ?", username).StructScan(entity)
-// 	if err != nil {
-// 		if errors.Is(err, sql.ErrNoRows) {
-// 			return nil, nil
-// 		}
+// FindByStatusID : IDからStatusを取得
+func (r *status) FindByStatusID(ctx context.Context, id string) (*object.Status, error) {
+	entity := new(object.Status)
+	err := r.db.QueryRowxContext(ctx, "select s.id, s.content, s.create_at, a.username as 'account.username', a.display_name as 'account.display_name', a.create_at as 'account.create_at' from status as s inner join account as a on s.account_id = a.id where s.id = ?", id).StructScan(entity)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
 
-// 		return nil, fmt.Errorf("%w", err)
-// 	}
+		return nil, fmt.Errorf("%w", err)
+	}
 
-// 	return entity, nil
-// }
+	return entity, nil
+}
 
 func (r *status) CreateStatus(ctx context.Context, account *object.Account, status *object.Status) (*object.Status, error) {
 	result, err := r.db.ExecContext(ctx, "insert into status (account_id, content) value (?, ?)", account.ID, status.Content)
--- app/domain/object/status.go
+++ app/domain/object/status.go
@@ -1,20 +1,12 @@
 package object
 
-// import (
-// 	"fmt"
-
-// 	"github.com/pkg/errors"
-// 	"golang.org/x/crypto/bcrypt"
-// )
-
 type (
 	StatusID    = int64
 
-	// Account account
 	Status struct {
 		ID StatusID `json:"id"`
 
-		Account *Account `json:"account"`
+		Account *Account `json:"account" db:"account"`
 
 		AccountId int64 `json:"-" db:"account_id"`
 
@@ -23,26 +15,3 @@ type (
 		CreateAt DateTime `json:"create_at,omitempty" db:"create_at"`
 	}
 )
-
-// // Check if given password is match to account's password
-// func (a *Account) CheckPassword(pass string) bool {
-// 	return bcrypt.CompareHashAndPassword([]byte(a.PasswordHash), []byte(pass)) == nil
-// }
-
-// // Hash password and set it to account object
-// func (a *Account) SetPassword(pass string) error {
-// 	passwordHash, err := generatePasswordHash(pass)
-// 	if err != nil {
-// 		return fmt.Errorf("generate error: %w", err)
-// 	}
-// 	a.PasswordHash = passwordHash
-// 	return nil
-// }
-
-// func generatePasswordHash(pass string) (PasswordHash, error) {
-// 	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		return "", fmt.Errorf("hashing password failed: %w", errors.WithStack(err))
-// 	}
-// 	return PasswordHash(hash), nil
-// }
